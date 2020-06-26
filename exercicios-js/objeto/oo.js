3865f2d3e29e237da1a379454f632e2db56e5c07
Lucas <lucasatletico@hotmail.com>
2020-06-26 16:49:48 -0300

    adicionando capitulo de objetos com : Constantes, Getters/Setters, Herança, JSON e Classes

---
+++ exercicios-js/objeto/oo.js
@@ -0,0 +1,15 @@
+//Procedural
+processamento(valor1, valor2,valor3)
+
+
+//orientado objeto
+objeto ={
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+
+    }
+}
+
+objeto.processamento()
