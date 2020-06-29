cd10e8a42d4f6e049ae938026c2a196a8530eeef
Lucas <lucasatletico@hotmail.com>
2020-06-29 14:14:20 -0300

    adicionando classes do capitulo ESNext com operadores: Rest/Spread, Tagged Template, Map, Set, For Of, Promisses, Callbacks, Async/Await,

---
+++ exercicios-js/esNext/promisse.js
@@ -0,0 +1,14 @@
+const { reject } = require("lodash");
+
+function falarDepoisDe(segundos, frase){
+    return new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            resolve(frase)
+        }, segundos * 1000)
+    })
+}
+
+falarDepoisDe(3, 'Que legal!')
+    .then(frase => frase.concat('?!?'))
+    .then(outrafrase => console.log(outrafrase))
+    .catch(e => console.log(e))
