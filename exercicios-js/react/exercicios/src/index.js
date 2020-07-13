a515eb48e9d415dcda16895fe93abb35b1f42e9c
Lucas <lucasatletico@hotmail.com>
2020-07-13 11:50:00 -0300

    capitulo sobre React: criando um projeto, criação de componentes, componentes (parametro, classe, Pai e filho)

---
+++ exercicios-js/react/exercicios/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+//import Primeiro from './componentes/Primeiro'
+//import BomDia from './componentes/BomDia'
+//import Multi, {BoaNoite} from './componentes/Multiplos'
+//import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
+ReactDOM.render(
+<div>
+    <Pai nome="Paulo" sobrenome="Silva"/>
+    <Filho nome="Pedro" sobrenome="Silva"/>
+    <Filho nome="Paulo" sobrenome="Silva"/>
+    <Filho nome="Carla" sobrenome="Silva"/>
+</div>
+, document.getElementById('root'))
