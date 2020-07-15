4432aaaf882bc34ec21a1a3a5c6e493e83a92880
Lucas <lucasatletico@hotmail.com>
2020-07-15 16:46:53 -0300

    capitulo sobre React: criando um projote de uma pagina de cadastro de usuarios

---
+++ exercicios-js/react/crud/frontend/src/components/main/App.jsx
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import React from 'react'
+
+
+import Logo from '../template/Logo'
+import Nav from '../template/Nav'
+import Main from '../template/Main'
+import Footer from '../template/Footer'
+
+export default props =>
+    <div className="app">
+        <Logo />
+        <Nav />
+        <Main icon="home" title="Inicio"
+        subtitle="Segundo Projeto do capitulo de React"/>
+        <Footer />
+        
+    </div>
---
+++ exercicios-js/react/crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import '../template/Footer.css'
+import React from 'react'
+
+export default props =>
+<footer className="footer">
+    <span>
+         Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+    <strong> Lucas</strong> 
+    </span>
+</footer>
