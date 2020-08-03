8fd54afc29f98a6345e80f2a28f58f858b06a2c2
Lucas <lucasatletico@hotmail.com>
2020-08-03 16:23:03 -0300

    projeto criação de uma pagina problema monty hall escolhas de portas em Vue JS

---
+++ exercicios-js/vue/montyhall/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id ="app">
+        <h1>Problema de monty hall</h1>
+        <Gift />
+    </div>
+</template>
+
+<script>
+import Gift from './components/Gift'
+export default {
+    name: 'App',
+    components: {Gift}
+}
+</script>
+
+<style>
+*{
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+}
+body{
+    color: #fff;
+    background: linear-gradient(to right, rgb(21, 153, 87), rgb(21, 87, 153));
+}
+#app{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+#app h1{
+    border: 1px solid #000;
+    background-color: #0004;
+    margin-bottom: 60px;
+}
+</style>
