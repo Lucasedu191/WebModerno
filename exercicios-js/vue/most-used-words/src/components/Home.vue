88f7ac3c380f15568d6ab26d1a597b68bde69c5f
Lucas <lucasatletico@hotmail.com>
2020-08-03 19:46:10 -0300

    projeto Desktop com JavaScript, Electron JS e VueJS

---
+++ exercicios-js/vue/most-used-words/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        Home
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    components: {
+        Home
+    },
+}
+</script>
+
+<style>
+
+</style>
